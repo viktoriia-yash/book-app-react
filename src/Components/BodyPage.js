194e2e840401cc3f82494e0c4d86f058b8342592
viktoriia-yash <yashkina.vic@gmail.com>
2023-06-27 20:49:16 +0300

    New component (books preview) + basic styling

--- src/Components/BodyPage.js
+++ src/Components/BodyPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Card from "./Card";
 
 const BodyPage = () => {
   return (
     <div className="body-page">
-      <div>BodyPage</div>
+      <div className="container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="" alt="" />
+        <div className="book-details">
+          <h3 className="title">Hi</h3>
+          <p className="cost">21</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
